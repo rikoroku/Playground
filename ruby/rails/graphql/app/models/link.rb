cdb342d8b9c2984aa6bec3edae4d9a19c83f9785
rikoroku <rikoroku55@gmail.com>
2020-12-30 22:36:11 +0900

    Revert "[add] user_id to link"
    This reverts commit 750eef1eebdb149a2c4ab015d202b2de9dbd57c6.

--- ruby/rails/graphql/app/models/link.rb
+++ ruby/rails/graphql/app/models/link.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class Link < ApplicationRecord
-  belongs_to :user
 end
--- ruby/rails/graphql/db/migrate/20201228060555_add_user_id_to_link.rb
+++
@@ -1,9 +0,0 @@
-class AddUserIdToLink < ActiveRecord::Migration[6.1]
-  def self.up
-    add_reference :links, :user
-  end
-
-  def self.down
-    remove_column :links, :user_id
-  end
-end
