09b7db85c524317763ec20636de76f120995dc97
rikoroku <rikoroku55@gmail.com>
2021-01-24 22:26:46 +0900

    WIP add: sum_of_subsets

---
+++ ruby/algorithms/backtracking/sum_of_subsets.rb
@@ -0,0 +1,11 @@
+# Overview: In this problem, there is a given set with some integer elements. And another some value is also provided,
+#           we have to find a subset of the given set whose sum is the same as the given sum value.
+# Ref: https://www.tutorialspoint.com/Subset-Sum-Problem
+# Time-complexity: O(2^N)
+# Visualizer: https://algorithm-visualizer.org/backtracking/sum-of-subsets
+
+set = [10, 7, 5, 18, 12, 20, 15]
+sum = 35
+
+def find_subsets_with_backtracking(set, sum)
+end
